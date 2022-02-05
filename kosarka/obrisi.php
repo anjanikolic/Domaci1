86f64acdd55972373502259c413d86e913873d73
anjanikolic <76402375+anjanikolic@users.noreply.github.com>
2022-02-05 12:57:31 +0100

    Delete kosarka directory

--- kosarka/obrisi.php
+++
@@ -1,8 +0,0 @@
-<?php
-include('inicijalizacija.php');
-$id = $_GET['id'];
-
-$db->where("mecID", $id);//za mec sa ovim id-om brisemo 
-$db->delete('mec');
-header("Location: tabelaTimovi.php");//sluzi da nas vrati na stranicu
-?>
